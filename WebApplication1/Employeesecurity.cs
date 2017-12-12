37146b32c25658bd23b2c780cf2cdd5403d0914d
Manuel <manuepeva@gmail.com>
2017-12-12 11:29:03 -0500

    merging

---
+++ WebApplication1/Employeesecurity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Employeesecurity
+    {
+        public static bool Login(string username, string password)
+        {
+            using(MyConnectionDB entities = new MyConnectionDB())
+            {
+                return entities.Users.Any(username => user.username.equals(username,
+                    StringComparison.OrdinalIgnoreCase) && user.Password == password);
+                    ))
+            }
+        }
+    }
+}
